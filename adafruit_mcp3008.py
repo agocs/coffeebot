5e581c0fad9d3d8579ad375f00d33bdac36bdee2
borromakot <zachary.s.daniel@gmail.com>
2014-01-22 11:29:17 -0600

    Modified weighing module
    Created Coffee Pot class, to allow for multiple coffee pots, and easier
    modifications.

--- adafruit_mcp3008.py
+++ adafruit_mcp3008.py
@@ -3,43 +3,73 @@ import time
 import os
 import RPi.GPIO as GPIO
 
+
 GPIO.setmode(GPIO.BCM)
 DEBUG = 1
 
-# read SPI data from MCP3008 chip, 8 possible adc's (0 thru 7)
-def readadc(adcnum, clockpin, mosipin, misopin, cspin):
-        if ((adcnum > 7) or (adcnum < 0)):
-                return -1
-        GPIO.output(cspin, True)
+SPICLK = 18
+SPIMISO = 23
+SPIMOSI = 24
+SPICS = 25
 
-        GPIO.output(clockpin, False)  # start clock low
-        GPIO.output(cspin, False)     # bring CS low
+class CoffeePot(self,name, adcnum, tolerance):
+    self.adcnum = adcnum
+    self.sensor_changed = 0
+    self.last_read = 0
+    self.tolerance = tolerance
+    self.name = name
+    #Pretty function to run readadc
+    def __str__(self):
+        return self.name
+    
+    def getWeight(self):
+        current_read = self.readadc(adcnum, SPICLK, SPIMISO, SPIMOSI, SPICS)
+        if abs(current_read - last_read) > tolerance:
+            self.last_read = current_read
+        return self.last_read
+    
+    
+    
+    #gets our weight
+    def readadc(self, adcnum, clockpin, mosipin, misopin, cspin):
+            if ((adcnum > 7) or (adcnum < 0)):
+                    return -1
+            GPIO.output(cspin, True)
 
-        commandout = adcnum
-        commandout |= 0x18  # start bit + single-ended bit
-        commandout <<= 3    # we only need to send 5 bits here
-        for i in range(5):
-                if (commandout & 0x80):
-                        GPIO.output(mosipin, True)
-                else:
-                        GPIO.output(mosipin, False)
-                commandout <<= 1
-                GPIO.output(clockpin, True)
-                GPIO.output(clockpin, False)
+            GPIO.output(clockpin, False)  # start clock low
+            GPIO.output(cspin, False)     # bring CS low
 
-        adcout = 0
-        # read in one empty bit, one null bit and 10 ADC bits
-        for i in range(12):
-                GPIO.output(clockpin, True)
-                GPIO.output(clockpin, False)
-                adcout <<= 1
-                if (GPIO.input(misopin)):
-                        adcout |= 0x1
+            commandout = adcnum
+            commandout |= 0x18  # start bit + single-ended bit
+            commandout <<= 3    # we only need to send 5 bits here
+            for i in range(5):
+                    if (commandout & 0x80):
+                            GPIO.output(mosipin, True)
+                    else:
+                            GPIO.output(mosipin, False)
+                    commandout <<= 1
+                    GPIO.output(clockpin, True)
+                    GPIO.output(clockpin, False)
 
-        GPIO.output(cspin, True)
+            adcout = 0
+            # read in one empty bit, one null bit and 10 ADC bits
+            for i in range(12):
+                    GPIO.output(clockpin, True)
+                    GPIO.output(clockpin, False)
+                    adcout <<= 1
+                    if (GPIO.input(misopin)):
+                            adcout |= 0x1
+
+            GPIO.output(cspin, True)
         
-        adcout >>= 1       # first bit is 'null' so drop it
-        return adcout
+            adcout >>= 1       # first bit is 'null' so drop it
+            return adcout
+    
+coffee_pot_left = CoffeePot("Left", 0, 5)
+coffee_pot_right = CoffeePot("Right", 1, 5)
+
+# read SPI data from MCP3008 chip, 8 possible adc's (0 thru 7)
+
 
 # change these as desired - they're the pins connected from the
 # SPI port on the ADC to the Cobbler
@@ -55,34 +85,23 @@ GPIO.setup(SPICLK, GPIO.OUT)
 GPIO.setup(SPICS, GPIO.OUT)
 
 # 10k trim pot connected to adc #0
-potentiometer_adc = 0;
 
-last_read = 0       # this keeps track of the last potentiometer value
-tolerance = 5       # to keep from being jittery we'll only change
+#last_read = 0       # this keeps track of the last potentiometer value
+#tolerance = 5       # to keep from being jittery we'll only change
                     # volume when the pot has moved more than 5 'counts'
 
 while True:
         # we'll assume that the pot didn't move
-        trim_pot_changed = False
 
         # read the analog pin
-        trim_pot = readadc(potentiometer_adc, SPICLK, SPIMOSI, SPIMISO, SPICS)
-        # how much has it changed since the last read?
-        pot_adjust = abs(trim_pot - last_read)
+        left_weight = coffee_pot_left.getWeight()
+        right_weight = coffee_pot_right.getWeight()
+
 
         if DEBUG:
-                print "trim_pot:", trim_pot
-                print "pot_adjust:", pot_adjust
-                print "last_read", last_read
+                print "Coffee Pot: " + coffee_pot_left + " weighs" + coffee_pot_left.getWeight() 
+                print "Coffee Pot: " + coffee_pot_right + " weighs" + coffee_pot_right.getWeight() 
 
-        if ( pot_adjust > tolerance ):
-               trim_pot_changed = True
-
-        if DEBUG:
-                print "trim_pot_changed", trim_pot_changed
-
-	if ( trim_pot_changed ):
-		last_read = trim_pot
 
         # hang out and do nothing for a half second
         time.sleep(0.5)
