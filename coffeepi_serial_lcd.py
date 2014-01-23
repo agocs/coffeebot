e82aae16a6f5ea425d9e6c87842d47bdfa123888
Ryan Bibbey <rbibbey@gmail.com>
2014-01-22 21:29:37 -0600

    change cursor behavior
    sorry for so many checkins...

--- coffeepi_serial_lcd.py
+++ coffeepi_serial_lcd.py
@@ -14,8 +14,9 @@ class Serial_LCD:
 		if self.lcd.isOpen():
 			self.lcd.write(chr(32) * 32)
 			self.lcd.write(chr(12))
-			self.lcd.write(chr(17))
-			time.sleep(1)
+			time.sleep(5)
+			self.lcd.write(chr(22))
+			
 			self.lcd.write(chr(128))
 			self.lcd.write(self.program)
 			self.lcd.write(chr(148))
