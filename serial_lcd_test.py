235297e254a219dd73e1a1f383f7b9fe7beccd23
Ryan Bibbey <rbibbey@gmail.com>
2014-01-22 20:54:16 -0600

    serial lcd test

---
+++ serial_lcd_test.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+from coffeepi_serial_lcd import Serial_LCD
+
+program = "CoffeePi v1.0"
+
+lcd = Serial_LCD('/dev/ttyAMA0',19200,program)
+
+temp_dict = {}
+temp_dict["pot"] = 1
+temp_dict["lastBrew"] = "1/1/1900" 
+temp_dict["currentLevel"] = 0.54
+temp_dict["removed"] = false
+
+lcd.writeToLcd(temp_dict)
