72284919a4c3fd03ad1cab62fd3a5c62fcf0cfe9
camerazn <christopheragocs@cagocs.local>
2014-01-22 21:17:57 -0600

    hash tag somebs

--- coffeepi_controller.py
+++ coffeepi_controller.py
@@ -37,8 +37,8 @@ class coffee_pot:
         if value > self.full and self.removed:
             self.lastbrew = time.time()
         self.removed = temp_current_level < self.off
-        thiscurrentvalue = min(temp_current_level, self.max)
-        thiscurrentvalue = max(thiscurrentvalue, self.min)
+        thiscurrentvalue = min(temp_current_level, self.full)
+        thiscurrentvalue = max(thiscurrentvalue, self.max)
         self.post_value = (thiscurrentvalue - self.empty) / (self.max - self.empty)
         
 left = coffee_pot("1", full=90, empty = 71, off=10, max=90) 
