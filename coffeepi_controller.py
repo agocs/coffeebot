cf8a0f2d97064f5179421775c814d139400bf7d7
Ryan Bibbey <rbibbey@gmail.com>
2014-02-25 03:04:42 -0800

    Exception handling, logging and cleanup
    Moved coffee_pot class to its own file; removed internal refs
    Added logging support to coffeepi_controller
    Added logging support to coffee_pot
    Renamed serial lcd class and code file
    Cleaned up imports in coffeepi_controller
    Exception handling in coffeepi_controller
    Reading data clipping when above/below max/min
    Removed current_level and fixed typo in postvalue props in coffee_pot
    class
    Set lastbrew time to > 24h before to force OLD display on startup
    Added comments to coffee_pot with suggestions for fixes

--- coffeepi_controller.py
+++ coffeepi_controller.py
@@ -1,103 +1,83 @@
 #!/usr/bin/env python
+#This is the controller for our CoffeePi
 from __future__ import division
 import time
 from collections import deque
 import urllib
 import urllib2
 import json
-from coffeepi_serial_lcd import Serial_LCD
-lcd = Serial_LCD('/dev/ttyAMA0', 19200)
-#This is the controller for our CoffeePi
-
+import logging
 import adafruit_mcp3008
+import coffee_pot
+import coffeepi_serial_lcd
 
+#defaults for readings off of sensors
+valid_data_min = 20
+valid_data_max = 125
+
+#Setup logging
+logging.basicConfig(file='coffee_bot_3000.log',level=logging.DEBUG,format='%(asctime)s %(levelname)s %(message)s')
+
+logging.info('Initializing LCD interface...')
+lcd = coffeepi_serial_lcd('/dev/ttyAMA0', 19200)
+
+logging.info('Initializing coffee pot objects...')
 coffee_pots={}
+coffee_pots["1"] = coffee_pot("1", full=70, empty = 35, off = valid_data_min, max=valid_data_max)
+coffee_pots["2"] = coffee_pot("2", full=70, empty = 35, off = valid_data_min, max=valid_data_max)
 
-minimum_valid_data = 0
-maximum_valid_data = 700
-
-
-## important, the max of a pot is the fullest we will consider the pot to be ever, but the full amount is how heavy
-## it needs to be in order to be considered full by our controller.
-
-class coffee_pot:
-    def __init__(self, name, full=400, empty=50, off=0, max=500.00):
-        self.name = name
-        self.values = [0,0,0,0,0,0,0,0,0,0]
-        self.lastbrew = time.time()
-        self.current_level = 0
-        self.removed = False
-        self.full = full
-        self.empty = empty
-        self.off = off
-        self.post_value = 0
-        self.max = max
-        coffee_pots[name] = self
-
-    def addReading(self, value):
-        self.values.pop(0)
-        self.values.append(value)
-        if value > self.full and self.removed:
-            self.lastbrew = time.time()
-        self.removed = value < self.off
-        print "Coffee Pot: " + self.name + " is reading " + str(value) + "\n"
-
-
-    def getPostValue(self):
-        temp_current_level = float(reduce(lambda x, y: x + y, self.values) / float(len(self.values)))
-
-        self.postvalue = float((float(temp_current_level - self.empty)) / (float(int(self.max) - self.empty)))
-
-        self.postvalue = min(self.postvalue, 1)
-        self.postvalue = max(self.postvalue, 0)
-
-        return self.postvalue
-
-
-left = coffee_pot("1", full=70, empty = 35, off = 20, max=125)
-right = coffee_pot("2", full=70, empty = 35, off = 20, max=125)
-
-
-
+logging.info('Entering main loop...')
 count = 1
 while True:
 
+	try:
+		logging.info('Reading sensors values...')
+		readings = adafruit_mcp3008.getWeights()
+		
+		for reading in readings:
+			value = readings[reading]
+			
+			if valid_data_min > value:
+				value = valid_data_min
+				
+			if valid_data_max < value
+				value = valid_data_max
 
-    readings = adafruit_mcp3008.getWeights()
-    for reading in readings:
-        if minimum_valid_data > readings[reading] or maximum_valid_data < readings[reading]:
-            continue
-        coffee_pots[reading].addReading(readings[reading])
+			coffee_pots[reading].addReading(value)
+	except:
+		logging.exception('Error occurred during read and setting values from sensors')
+		#Consider here performing a clean exit; then configure the script to respawn if it dies?
 
-    if count % 10 == 0:
-        to_post = {"update":[]}
-        for item in coffee_pots:
-            temp_dict = {}
-            temp_dict["pot"] = coffee_pots[item].name
-            temp_dict["lastBrew"] = coffee_pots[item].lastbrew
-            temp_dict["currentLevel"] = coffee_pots[item].getPostValue()
-            temp_dict["removed"] = coffee_pots[item].removed
-            to_post["update"].append(temp_dict)
-            print to_post["update"]
-        ##POST HERE
-        url = 'http://coffeemonitor-backstopcoffee.rhcloud.com/pots/update'
-        params = json.JSONEncoder().encode(to_post)
-        headers ={'Content-type': "application/json"}
-        req = urllib2.Request(url, params, headers)
-        try:
-            response = urllib2.urlopen(req).read()
-        except urllib2.HTTPError, error:
-            print error
-            print "it failed."
-            contents = error.read()
-        lcd.writeToLcd(to_post["update"])
-        count = 1
+	try:
+		if count % 10 == 0:
+			logging.info('Setting up data dictionary...')
+			to_post = {"update":[]}
+			for item in coffee_pots:
+				temp_dict = {}
+				temp_dict["pot"] = coffee_pots[item].name
+				temp_dict["lastBrew"] = coffee_pots[item].lastbrew
+				temp_dict["currentLevel"] = coffee_pots[item].getPostValue()
+				temp_dict["removed"] = coffee_pots[item].removed
+				to_post["update"].append(temp_dict)
+				logging.info('Data dictionary created: %s', to_post["update"])
+				
+			##POST HERE
+			try:
+				logging.info('Preparing to post data to coffee monitor site...')
+				url = 'http://coffeemonitor-backstopcoffee.rhcloud.com/pots/update'
+				params = json.JSONEncoder().encode(to_post)
+				headers ={'Content-type': "application/json"}
+				req = urllib2.Request(url, params, headers)
 
+				response = urllib2.urlopen(req).read()
+			except urllib2.HTTPError, error:
+				logging.exception('Error occurred while attempting to post an update to the web service')
+				contents = error.read()
+				
+			lcd.writeToLcd(to_post["update"])
+			count = 1
+	except:
+		logging.exception('Error occurred while attempting to process the sensor readings')
 
     count = count + 1
     time.sleep(1)
-
-
-
-    #
-
